da6523619210d0b9c1e3758ab749179f68215cfa
Seonghyun Jung <tjdgusus@gmail.com>
2021-08-02 19:47:09 +0900

    백준 2869번 시간초과 부분 수정

--- Baekjoon/Baekjoon2869/baekjoon2869.cpp
+++ Baekjoon/Baekjoon2869/baekjoon2869.cpp
@@ -6,18 +6,22 @@ int main() {
 	int climb;	// A(m)
 	int lose;	// B(m)
 	int goal;	// V(m)
-	int total = 0;
-	int dayCount = 0;
+	int dayCount = 1;
 
 	cin >> climb >> lose >> goal;
 
-	while (1) {
-		total += climb;
+	if (goal <= climb) {
+		cout << dayCount << endl;
+		return 0;
+	}
+
+	goal = goal - climb;
+
+	dayCount += goal / (climb - lose);
+
+	// goal의 나머지가 있다는 것은 아직 남은 거리가 있다는 것이기 때문에 예외 처리
+	if ((goal % (climb - lose)) != 0) {
 		dayCount++;
-
-		if (total >= goal) break;
-
-		total -= lose;
 	}
 
 	cout << dayCount << endl;
